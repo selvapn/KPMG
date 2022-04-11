5ed785130bbbcb693b553932d6f3e791defaed36
selvapn <selva.pn@gmail.com>
2022-04-11 14:23:15 +0100

    get metadata

---
+++ metadata.ps1
@@ -0,0 +1,12 @@
+﻿Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "http://169.254.169.254/latest/user-data/" | ConvertTo-Json -Depth 64 | Out-File data.json
+$jsondata = get-Content .\data.json | ConvertFrom-Json
+if (test-path -Path data.json -ErrorAction Stop) {
+    $check = Read-Host "Enter data needs to be fetched in json"
+    foreach ($data in $jsondata.psobject.Properties.name) {
+        if ($jsondata.$data.psobject.Properties.name -eq $check){
+           write-host "Data $check : Value :$($jsondata.$data.$check)"
+        }
+    }
+}
+else {write-host "Json file hasnt downloaded"}
+
